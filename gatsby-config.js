562f1aaec0214bfc51718bad777fc75385975ffe
manavchawla3 <manavchawla3@gmail.com>
2020-07-23 02:10:58 +0530

    Added: Gatsby-contentful plugin Added: dotenv package Modified: prettierrc to enable semicolons

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
+const dotenv = require("dotenv");
+
+if (process.env.NODE_ENV === "development") {
+  dotenv.config();
+} else {
+  dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+}
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Manav Chawla - RESUME`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@manavchawla3`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +35,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `${process.env.CONTENTFUL_SPACE_ID}`,
+        accessToken: `${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
